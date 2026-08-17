031696026972fd43febe44f010b6ff53d792ad01
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-17 16:17:26 +0545

    Navbar component modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { MapPin } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { FaCaretDown } from 'react-icons/fa';
 import { IoCart, IoCartOutline } from 'react-icons/io5';
 import { Link, NavLink } from 'react-router-dom'
@@ -10,9 +10,13 @@ import {
   SignOutButton,
   UserButton,
 } from '@clerk/clerk-react'
+import { CgClose } from 'react-icons/cg';
 
-const Navbar = () => {
-  const location = false;
+const Navbar = ({ location, getLocation,
+  openDropdown,
+  toggelDropdown }) => {
+
+
   return (
     <section className='bg-white shadow-2xl py-3'>
       <div className='max-w-6xl mx-auto flex justify-between items-center '>
@@ -25,10 +29,18 @@ const Navbar = () => {
           </Link>
 
           {/* location  */}
-          <div className='flex items-center gap-1 text-gray-700 cursor-pointer'>
-            <MapPin className='text-red-500' />
-            <span className='font-semibold'>{location ? <div><span>Kathmandu, Nepal</span></div> : "Add Address"}</span>
-            <FaCaretDown />
+          <div className='flex items-center gap-1 text-gray-700'>
+            <MapPin className='text-red-500  cursor-pointer' onClick={toggelDropdown} />
+            <span className='font-semibold  cursor-pointer' onClick={toggelDropdown}>{location ? <div className='-space-y-2' >
+              <p>{location.neighbourhood}</p>
+              <p>{location.city_district}</p></div> : "Add Address"}</span>
+            <FaCaretDown onClick={toggelDropdown} className=' cursor-pointer' />
+            {
+              openDropdown && <div className='w-64 shadow-2xl p-4 border-2 bg-white border-gray-100 fixed z-50 rounded-md top-16 left-60'>
+                <h1 className='text-xl flex justify-between items-center'>Change Location <span><CgClose onClick={toggelDropdown} className='cursor-pointer' /> </span></h1>
+                <button className='bg-red-500 text-white w-full py-1 rounded-lg text-xl  hover:bg-red-600 cursor-pointer duration-300 mt-4' onClick={getLocation}>Detect my location</button>
+              </div>
+            }
           </div>
         </div>
 
@@ -49,7 +61,7 @@ const Navbar = () => {
 
           <div>
             <SignedOut>
-              <SignInButton mode="modal" />
+              <SignInButton mode="modal" className="bg-red-500 rounded-md text-white cursor-pointer px-4 py-1" />
             </SignedOut>
             <SignedIn>
               <UserButton />
