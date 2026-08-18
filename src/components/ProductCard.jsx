686b0393add78688025b4857f0be7daa8080064b
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-18 22:07:15 +0545

    Product Card Component added

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IoCartOutline } from 'react-icons/io5'
+
+const ProductCard = ({ product }) => {
+    return (
+        <div className='border border-gray-100 rounded-2xl hover:scale-105 cursor-pointer hover:shadow-2xl p-2 transition-all h-max'>
+            <img src={product.thumbnail} alt="image" className='bg-gray-300 aspect-square rounded-2xl' />
+            <h1 className='line-clamp-1 p-1 font-bold text-gray-800 sm:text-lg text-md mt-2'>{product.title}</h1>
+            <h2 className='mx-1 mb-1 line-clamp-1 font-semibold text-gray-800 sm:text-md text-sm'>{product.brand}</h2>
+            <span className='sm:text-lg text-lg text-red-500'>${product.price}</span>
+            <button className='text-white bg-red-500 hover:bg-red-600 cursor-pointer w-full rounded-md flex p-2 justify-center gap-2 items-center transition-all mt-2'><IoCartOutline className='w-6 h-6' /><span>Add to cart</span></button>
+        </div>
+    )
+
+}
+
+export default ProductCard
