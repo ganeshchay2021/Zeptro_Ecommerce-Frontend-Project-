51042a0a458eaf29fb6c83b6c53c191105067664
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-19 22:15:09 +0545

    Product Crums added

---
+++ src/components/BreadCrums.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products from '../Pages/Products'
+import { useNavigate } from 'react-router-dom'
+
+const BreadCrums = ({title}) => {
+    const navigate= useNavigate();
+  return (
+    <div className='my-10'>
+        <h1 className='sm:text-xl text-lg text-gray-700 font-semibold'><span className='cursor-pointer' onClick={()=> navigate('/home')}>Home</span> / <span className='cursor-pointer' onClick={()=> navigate('/products')}>Products</span> / <span>{title}</span> </h1>
+
+    </div>
+  )
+}
+
+export default BreadCrums
