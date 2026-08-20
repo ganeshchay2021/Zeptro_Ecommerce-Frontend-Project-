d247b939fb306115f292239fca5ea09e7eadfdad
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-08-20 21:21:23 +0545

    Protected Route Component added

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -11,11 +11,11 @@ const ProtectedRoute = ({ children }) => {
                 user ? children : <>
                     <div className='max-w-6xl mx-auto px-4 h-screen w-full flex justify-center items-center'>
                         <div className='text-center space-y-4'>
-                            <Link to="/" >
+                            <div>
                                 <h1 className='lg:text-5xl md:text-4xl text-3xl font-bold'>
                                     <span className='text-red-500 font-serif'>Swift</span>Bazaar
                                 </h1>
-                            </Link>
+                            </div>
                             <div className='space-y-4'>
                                 <h1 className='lg:text-3xl md:text-2xl text-xl text-gray-800 font-semibold'>To access cart</h1>
                                 <p className='lg:text-xl md:text-lg text-sm text-gray-600'>Please <span className='font-semibold'>sign in</span> your account first</p>
